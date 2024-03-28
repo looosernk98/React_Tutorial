377ef613a68d993fba496feb58830e7b6c88ae17
Niranjan Kumar <niranjan.kumar@talent500.com>
2024-03-28 17:12:47 +0530

    first commit

---
+++ src/home/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+// JSX
+
+const Home = () => {
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <p>lorem isdoisdkgdgjh hjdgjhdfjehdfjehfdjeh gjhefgjegfjehfgejhf </p>
+        </div>
+    )
+}
+
+// function Home(){
+//     return(
+//         <div>
+//             <h1>Home Page</h1>
+//             <p>lorem isdoisdkgdgjh hjdgjhdfjehdfjehfdjeh gjhefgjegfjehfgejhf </p>
+//         </div>
+//     )
+// }
+
+export default Home
+
+
+
