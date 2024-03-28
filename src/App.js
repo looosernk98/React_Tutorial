64567a8db7a23e4ea7b947f7c8bf416dfe81160c
Niranjan Kumar <niranjan.kumar@talent500.com>
2024-03-28 17:12:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Home from './home/index.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,19 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <h1>Niranjan</h1>
+      <h2>Hello world</h2>
+
+      <Home/>
+      <Home/>
+
+      {/* <div>
+            <h1>Home Page</h1>
+            <p>lorem isdoisdkgdgjh hjdgjhdfjehdfjehfdjeh gjhefgjegfjehfgejhf </p>
+      </div> */}
+
     </div>
   );
 }
